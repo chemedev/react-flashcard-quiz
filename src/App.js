198ad5985fc39b52c99c68618d65862a090e0c64
jchemell <juanchemell@gmail.com>
2020-04-12 00:03:34 -0300

    First commit, finished project from WDS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import FlashcardList from './FlashcardList';
+import './app.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [flashcards, setFlashcards] = useState([]);
+	const [categories, setCategories] = useState([]);
+
+	const categoryEl = useRef();
+	const amountEl = useRef();
+
+	useEffect(() => {
+		axios.get('https://opentdb.com/api_category.php').then((res) => {
+			setCategories(res.data.trivia_categories);
+		});
+	}, []);
+
+	// useEffect(() => {}, []);
+
+	function decodeString(str) {
+		const textArea = document.createElement('textarea');
+		textArea.innerHTML = str;
+		return textArea.value;
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		axios
+			.get('https://opentdb.com/api.php', {
+				params: {
+					amount: amountEl.current.value,
+					category: categoryEl.current.value,
+				},
+			})
+			.then((res) => {
+				setFlashcards(
+					res.data.results.map((questionItem, index) => {
+						const answer = questionItem.correct_answer;
+						const options = [
+							...questionItem.incorrect_answers.map((a) => decodeString(a)),
+							decodeString(answer),
+						];
+						return {
+							id: `${index}-${Date.now()}`,
+							question: decodeString(questionItem.question),
+							answer: questionItem.correct_answer,
+							options: options.sort(() => Math.random() - 0.5),
+						};
+					})
+				);
+			});
+	}
+
+	return (
+		<>
+			<form className="header" onSubmit={handleSubmit}>
+				<div className="form-group">
+					<label htmlFor="category">Category</label>
+					<select id="category" ref={categoryEl}>
+						{categories.map((category) => {
+							return (
+								<option value={category.id} key={category.id}>
+									{category.name}
+								</option>
+							);
+						})}
+					</select>
+				</div>
+				<div className="form-group">
+					<label htmlFor="amount">Number Of Questions</label>
+					<input
+						type="number"
+						id="amount"
+						min="1"
+						step="1"
+						defaultValue={10}
+						ref={amountEl}
+					/>
+				</div>
+				<div className="form-group">
+					<button className="btn">Generate</button>
+				</div>
+			</form>
+			<div className="container">
+				<FlashcardList flashcards={flashcards} />
+			</div>
+		</>
+	);
 }
+
+// const SAMPLE_FLASHCARDS = [
+// 	{
+// 		id: 1,
+// 		question: 'What is 2 + 2?',
+// 		answer: '4',
+// 		options: ['2', '3', '4', '5'],
+// 	},
+// 	{
+// 		id: 2,
+// 		question: 'Question 2?',
+// 		answer: 'Answer',
+// 		options: ['Answer 1', 'Answer 2', 'Answer 3'],
+// 	},
+// ];
 
 export default App;
